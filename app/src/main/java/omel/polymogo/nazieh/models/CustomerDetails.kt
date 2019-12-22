cbca87df03decd9bba9e7f601f3cd13ad76b66e9
omelnour <michaelwagih199@gmail.com>
2019-12-22 12:34:54 +0200

    22-12

--- app/src/main/java/omel/polymogo/nazieh/models/CustomerDetails.kt
+++ app/src/main/java/omel/polymogo/nazieh/models/CustomerDetails.kt
@@ -1,6 +1,21 @@
 package omel.polymogo.nazieh.models
 
-class CustomerDetails(val id: String?, val name:String, val phone:String ){
-  constructor():this("","","")
+class CustomerDetails(
+
+    val id: String?, val totalValue: Double, val content: String,
+    val contractorName: String,
+    val Date: String,
+    val ReceivingAmount: Double, val RemaininAmount: Double,
+    val recipientName: String
+)
+{
+
+    constructor() : this(
+        "", 0.0,
+        "", "",
+        "", 0.0,
+        0.0,
+        ""
+    )
 }
 
